418fe22b87c42748bfca93346ec6e568589595ef
Ford Fowler <fordjfowler@gmail.com>
2016-09-26 12:57:56 -0700

    create hours array; choose location names

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // return Math.floor(Math.random() * (max - min + 1)) + min
-// brainstorming: .render() ,
+// brainstorming: properties: min max customers per hour, hours (array), avg cookies per sale {7ish total props}
+//methods: .render(), generateHourlyCookies(), .generateDailyCookies(), .generateHourlyCustomers()
+var Hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+
+var location1stAndPike =
+var locationSeaTac = 
+var locationSeattleCenter =
+var locationCapHill =
+var locationAlki =
