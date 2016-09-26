46d577c07e71f38abcfc5a9068a46ace62b41355
Ford Fowler <fordjfowler@gmail.com>
2016-09-26 13:19:44 -0700

    write calculate hourly cookies

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // return Math.floor(Math.random() * (max - min + 1)) + min
 // brainstorming: properties: min max customers per hour, hours (array), avg cookies per sale {7ish total props}
-//methods: .render(), generateHourlyCookies(), .generateDailyCookies(), .generateHourlyCustomers()
+
 var hourlyList = [['6am'],['7am'],['8am'],['9am'],['10am'],['11am'],['12pm'],['1pm'],['2pm'],['3pm'],['4pm'],['5pm'],['6pm'],['7pm'],['8pm']];
 
 var location1stAndPike = {
@@ -11,15 +11,17 @@ var location1stAndPike = {
   render : function() {
 
   },
-  generateHourlyCookies : function() {
-    
+  calculateHourlyCookies : function(hrIndex) {
+    var hourlyCookies = hourlyList[hrIndex][1] * avgCookiesPerSale;
+    hourlyList[hrIndex][2] = hourlyCookies;
+    return hourlyCookies;
   },
-  generateDailyCookies : function() {
+  calculateDailyCookies : function() {
 
   },
-  generateHourlyCustomers : function(i) {
+  generateHourlyCustomers : function(hrIndex) {
     var randomNumCustomers = Math.floor(Math.random() * (maxCustomersPerHr - minCustomersPerHr + 1)) + minCustomersPerHr;
-    hourlyList[i][1] = randomNumCustomers;
+    hourlyList[hrIndex][1] = randomNumCustomers;
     return randomNumCustomers;
   }
 }
