2209adc8effd71ce1cae186f8029c1916e960eb1
Ford Fowler <fordjfowler@gmail.com>
2016-09-26 13:12:36 -0700

    write generateHourlyCustomers; make hours array 2-dimensional

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // return Math.floor(Math.random() * (max - min + 1)) + min
 // brainstorming: properties: min max customers per hour, hours (array), avg cookies per sale {7ish total props}
 //methods: .render(), generateHourlyCookies(), .generateDailyCookies(), .generateHourlyCustomers()
-var Hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+var hourlyList = [['6am'],['7am'],['8am'],['9am'],['10am'],['11am'],['12pm'],['1pm'],['2pm'],['3pm'],['4pm'],['5pm'],['6pm'],['7pm'],['8pm']];
 
 var location1stAndPike = {
   minCustomersPerHr : 23,
@@ -9,16 +9,18 @@ var location1stAndPike = {
   avgCookiesPerSale : 6.3,
 
   render : function() {
-    
+
   },
   generateHourlyCookies : function() {
-
+    
   },
   generateDailyCookies : function() {
 
   },
-  generateHourlyCustomers : function() {
-
+  generateHourlyCustomers : function(i) {
+    var randomNumCustomers = Math.floor(Math.random() * (maxCustomersPerHr - minCustomersPerHr + 1)) + minCustomersPerHr;
+    hourlyList[i][1] = randomNumCustomers;
+    return randomNumCustomers;
   }
 }
 var locationSeaTac =
