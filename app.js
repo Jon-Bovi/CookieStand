3f73e9814fb5b1a5a8ed6b5fcf0d8360130443a4
Ford Fowler <fordjfowler@gmail.com>
2016-09-26 13:02:52 -0700

    define and set necessary properties; define methods

--- app.js
+++ app.js
@@ -3,8 +3,25 @@
 //methods: .render(), generateHourlyCookies(), .generateDailyCookies(), .generateHourlyCustomers()
 var Hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
-var location1stAndPike =
-var locationSeaTac = 
+var location1stAndPike = {
+  minCustomersPerHr : 23,
+  maxCustomersPerHr : 65,
+  avgCookiesPerSale : 6.3,
+
+  render : function() {
+    
+  },
+  generateHourlyCookies : function() {
+
+  },
+  generateDailyCookies : function() {
+
+  },
+  generateHourlyCustomers : function() {
+
+  }
+}
+var locationSeaTac =
 var locationSeattleCenter =
 var locationCapHill =
 var locationAlki =
