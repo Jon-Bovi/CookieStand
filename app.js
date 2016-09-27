7b2086457c7e54667a3005f291f946e572cde39d
Ford Fowler <fordjfowler@gmail.com>
2016-09-27 09:51:12 -0700

    change background-image; new css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 // return Math.floor(Math.random() * (max - min + 1)) + min
 // brainstorming: properties: min max customers per hour, hours (array), avg cookies per sale {7ish total props}
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 
 var location1stAndPike = {
